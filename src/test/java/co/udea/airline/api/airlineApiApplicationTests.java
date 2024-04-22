daf7ad7550eab2529bb1345d61eaad94e792915f
WilmerSoto <wilmer.soto@udea.edu.co>
2024-04-21 20:46:55 -0500

    add test profile and update build.yaml

--- src/test/java/co/udea/airline/api/airlineApiApplicationTests.java
+++ src/test/java/co/udea/airline/api/airlineApiApplicationTests.java
@@ -1,11 +1,9 @@
 package co.udea.airline.api;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 import java.util.Date;
-import java.util.Optional;
 
 import co.udea.airline.api.controller.BookingController;
 import org.junit.jupiter.api.Test;
@@ -13,30 +11,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import co.udea.airline.api.model.jpa.model.*;
 import co.udea.airline.api.service.BookingService;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class AirlineApiApplicationTests {
     @Mock
     private BookingService bookingService;
 
     @InjectMocks
     private BookingController bookingController;
-
-    @LocalServerPort
-    private int puerto;
 
     @Test
     void contextLoads(){
